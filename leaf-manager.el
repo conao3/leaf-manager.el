3f1bcfe9670b6ed3afdd6ff4aed9342b0a26c463
conao3 <conao3@gmail.com>
2020-04-11 18:09:04 +0900

    show info when called interactively

--- leaf-manager.el
+++ leaf-manager.el
@@ -288,7 +288,9 @@ If FORCE is non-nil, read file even if cache is avairable."
 If FORCE is non-nil, load file if `leaf-manager-contents' is dirty state.
 See also `leaf-manager--contents'."
   (interactive)
-  (apply #'leaf-manager--contents force))
+  (apply #'leaf-manager--contents force)
+  (when (called-interactively-p 'interactive)
+    (message "leaf-manager: done!")))
 
 (defun leaf-manager-write-contents (&optional force)
   "Write `leaf-manager--contents' to `leaf-manager-file'.
